0df1bf8529c3614ab8cad2048fde2b71f22e5339
egorovin <egorovin@mail.ru>
2022-05-18 21:44:01 +0300

    homework 8

---
+++ src/main/java/Lesson8/Cat.java
@@ -0,0 +1,38 @@
+package Lesson8;
+
+public class Cat implements Participant {
+    private String name;
+    private int runningDistance;
+    private int jumpHeight;
+
+    public Cat(String name, int runningDistance, int jumpHeight) {
+        this.name = name;
+        this.runningDistance = runningDistance;
+        this.jumpHeight = jumpHeight;
+    }
+
+    @Override
+    public boolean jump(int height) {
+        if (jumpHeight >= height) {
+            System.out.println(this + " прыгает на высоту " + height);
+            return true;
+        }
+        System.out.println(this + " не смог прыгнуть на высоту " + height);
+        return false;
+    }
+
+    @Override
+    public boolean run(int distance) {
+        if (runningDistance >= distance) {
+            System.out.println(this + " пробегает дистанцию " + distance);
+            return true;
+        }
+        System.out.println(this + " не смог пробежать дистанцию " + distance);
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat " + name + " (max running " + runningDistance + " max jump " + jumpHeight + ")";
+    }
+}
---
+++ src/main/java/Lesson8/Participant.java
@@ -0,0 +1,4 @@
+package Lesson8;
+
+public interface Participant extends Jumper, Runner{
+}
